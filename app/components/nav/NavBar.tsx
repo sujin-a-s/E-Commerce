68d2c28f429035c5f677e6d1b610e8319d20ac5c
Sujin <sunilkumarsujin@gmail.com>
2024-07-30 23:02:26 +0530

    Conditional rendering of usermenu

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -3,10 +3,13 @@ import Container from "../Container";
 import { Redressed } from "next/font/google";
 import CartCount from "./CartCount";
 import UserMenu from "./UserMenu";
+import { getCurrentUser } from "@/actions/getCurrentUser";
 
 const redressed = Redressed({subsets : ["latin"], weight: ["400"]})
 
-const NavBar = () => {
+const NavBar = async() => {
+    const currentUser = await getCurrentUser()
+    
     return ( 
         <div className="sticky top-0 border z-50 w-full bg-slate-200  shadow-md">
             <div className="py-4 border border-b-[1px]">
@@ -16,7 +19,7 @@ const NavBar = () => {
                         <div className="hidden md:block">Search</div>
                         <div className="flex items-center gap-8 md:gap-12">
                             <CartCount />
-                            <UserMenu />
+                            <UserMenu currentUser={currentUser} />
                         </div>
                     </div>
                 </Container>
--- app/components/nav/UserMenu.tsx
+++ app/components/nav/UserMenu.tsx
@@ -7,9 +7,13 @@ import Link from "next/link"
 import MenuItem from "./MenuItem"
 import { signOut } from "next-auth/react"
 import BackDrop from "./BackDrop"
+import { SafeUser } from "@/types"
 
+interface UserMenuProps {
+    currentUser : SafeUser | null
+}
 
-const UserMenu = () => {
+const UserMenu:React.FC<UserMenuProps> = ({currentUser}) => {
         const [isOpen , setIsOpen] = useState(false)
 
         const toggleOpen = useCallback(()=>{
@@ -24,26 +28,32 @@ const UserMenu = () => {
             </div>
             {isOpen && (
                 <div className="absolute rounded-md shadow-md w-[170px] bg-white overflow-hidden right-0 top-12 text-sm flex flex-col cursor-pointer">
-                    <div>
-                        <Link href="/orders">
-                            <MenuItem onClick={toggleOpen}>Your Orders</MenuItem>
-                        </Link>
-                        <Link href="/admin">
-                            <MenuItem onClick={toggleOpen}>Admin</MenuItem>
-                        </Link>
-                        <MenuItem onClick={()=>{
-                            toggleOpen();
-                            signOut()
-                        }}>Logout</MenuItem>
-                    </div>
-                    <div>
-                        <Link href="/login">
-                            <MenuItem onClick={toggleOpen}>Login</MenuItem>
-                        </Link>
-                        <Link href="/register">
-                            <MenuItem onClick={toggleOpen}>Register</MenuItem>
-                        </Link>
-                    </div>
+                    {currentUser ? (
+                        <div>
+                            <Link href="/orders">
+                                <MenuItem onClick={toggleOpen}>Your Orders</MenuItem>
+                            </Link>
+                            <Link href="/admin">
+                                <MenuItem onClick={toggleOpen}>Admin</MenuItem>
+                            </Link>
+                            <hr />
+                            <MenuItem onClick={()=>{
+                                toggleOpen();
+                                signOut()
+                            }}>Logout</MenuItem>
+                    </div>                        
+                    ):(
+                        <div>
+                            <Link href="/login">
+                                <MenuItem onClick={toggleOpen}>Login</MenuItem>
+                            </Link>
+                            <Link href="/register">
+                                <MenuItem onClick={toggleOpen}>Register</MenuItem>
+                            </Link>
+                        </div>                       
+                    )}
+
+
                 </div>
             )}
         </div>
