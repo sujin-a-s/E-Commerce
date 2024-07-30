ba4e85ffce2456b79de1edbd16e48eab02400221
Sujin <sunilkumarsujin@gmail.com>
2024-07-30 11:38:54 +0530

    UI for Login Form

--- app/login/LoginForm.tsx
+++ app/login/LoginForm.tsx
@@ -8,11 +8,10 @@ import Button from "../components/Button";
 import Link from "next/link";
 import { AiOutlineGoogle } from "react-icons/ai";
 
-const RegisterForm = () => {
+const LoginForm = () => {
     const[isLoading,setIsLoading] = useState(false)
     const {register , handleSubmit , formState:{errors}} = useForm<FieldValues>({
         defaultValues : {
-            name : '',
             email : '',
             password : ''
         }
@@ -25,21 +24,20 @@ const onSubmit:SubmitHandler<FieldValues> = (data) => {
 
     return ( 
         <>
-        <Heading title="Sign up for E-shop" />
-        <Button outline label="Sign up with Google" icon={AiOutlineGoogle} onClick={()=>{}} />
+        <Heading title="Sign in to E-shop" />
+        <Button outline label="Continue with Google" icon={AiOutlineGoogle} onClick={()=>{}} />
         <hr className="bg-slate-300 w-full h-px"/>
-        <Input id="name" label="Name" disabled={isLoading} register={register} errors={errors} required />
         <Input id="email" label="Email" disabled={isLoading} register={register} errors={errors} required />
         <Input id="password" label="Password" disabled={isLoading} register={register} errors={errors} required type="password" />
-        <Button label={isLoading?"Loading":"Sign Up"} onClick={handleSubmit(onSubmit)} />
+        <Button label={isLoading?"Loading":"Login"} onClick={handleSubmit(onSubmit)} />
         <p className="text-sm">
-            Already have an account?{" "}
-            <Link className="underline" href="/login">
-                Log in
+            Do not have an account?{" "}
+            <Link className="underline" href="/register">
+                Sign in
             </Link>
         </p>
         </>
      );
 }
  
-export default RegisterForm;
+export default LoginForm;
